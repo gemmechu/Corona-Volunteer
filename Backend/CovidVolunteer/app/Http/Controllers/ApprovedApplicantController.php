00e3e2af0d4c5a7d36419dbaca8a75e445fb7d5b
unknown <eden.abdisa@yahoo.com>
2020-03-26 15:20:18 +0200

    Controller and model skeleton added

---
+++ Backend/CovidVolunteer/app/Http/Controllers/ApprovedApplicantController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ApprovedApplicant;
+use Illuminate\Http\Request;
+
+class ApprovedApplicantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ApprovedApplicant  $approvedApplicant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ApprovedApplicant $approvedApplicant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ApprovedApplicant  $approvedApplicant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ApprovedApplicant $approvedApplicant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ApprovedApplicant  $approvedApplicant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ApprovedApplicant $approvedApplicant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ApprovedApplicant  $approvedApplicant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ApprovedApplicant $approvedApplicant)
+    {
+        //
+    }
+}
