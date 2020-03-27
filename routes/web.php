76b8b0157aaa36d8236dfd49ef0feba9c6948252
unknown <eden.abdisa@yahoo.com>
2020-03-27 12:46:36 +0200

    Route fixing token problem arised

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/ActivityType','ActivityTypeController@index');
-Route::get('/ActivityType/{id}','ActivityTypeController@show');
-Route::post('/ActivityType','ActivityTypeController@store');
-Route::put('/ActivityType/{id}','ActivityTypeController@update');
-Route::delete('/ActivityType/{id}','ActivityTypeController@destroy');
 
-Route::get('Languages','LanguagesController@index');
-Route::get('Languages/{id}','LanguagesController@show');
-Route::post('Languages','LanguagesController@store');
-Route::put('Languages/{id}','LanguagesController@update');
-Route::delete('Languages/{id}','LanguagesController@destroy');
