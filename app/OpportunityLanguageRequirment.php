bc113ad0548a46e9393db7bef9599e3baccbc157
unknown <eden.abdisa@yahoo.com>
2020-03-28 20:29:50 +0200

    fixed opportunity

--- app/OpportunityLanguageRequirment.php
+++ app/OpportunityLanguageRequirment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class OpportunityLanguageRequirment extends Model
 {
   
-    protected $table = 'opportunity_language_requirement';
+    protected $table = 'opportunity_language_requirment';
 
             protected $primaryKey = 'opportunity_id';
             protected $fillable = [
