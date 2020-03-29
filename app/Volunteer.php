4789b506f5d47be2e8c3d49ab6c01f0627bae8b1
delilah14 <dilluxdes@gmail.com>
2020-03-29 04:01:44 +0300

    trying to authenticate with token

--- app/Volunteer.php
+++ app/Volunteer.php
@@ -24,7 +24,7 @@ class Volunteer extends Model
     parent::boot();
     self::creating(function ($model) {
         $model->id = (string) Uuid::generate(4);
-        $model->token = (string) Uuid::generate(8);
+        $model->token = (string) Uuid::generate(4);
     });
 }
     public function contact(){
