4d569537e5d3784cc575ee1f72819405052b20b1
delilah14 <dilluxdes@gmail.com>
2020-03-29 04:17:08 +0300

    trying to authenticate with token

--- config/auth.php
+++ config/auth.php
@@ -69,7 +69,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Volunteer::class,
+            'model' => App\User::class,
         ],
 
         // 'users' => [
